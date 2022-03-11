3717531d1e67eae4ad904f9a49bff6185288af51
MrWilliamA <will.archer@hotmail.com>
2022-03-11 16:03:58 +1100

    fin

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Code your solution in this file!
+
+const distanceFromHqInBlocks = (blocks) => {
+    const scuber = 42;
+    
+if(blocks > scuber) {
+    return blocks - scuber;
+
+} else if(scuber > blocks) {
+    return scuber - blocks;
+} else {
+    return "you're there";
+}
+    
+};
+
+const distanceFromHqInFeet = (feet) => {
+ 
+const scuberInFeet = distanceFromHqInBlocks(feet) * 264;
+const distance = Math.abs(scuberInFeet);
+if(distance != 0) {
+return distance;
+} 
+
+}
+
+const distanceTravelledInFeet = (currentPosition, destination ) => {
+
+    if(currentPosition > destination) {
+const differenceInFeet = (currentPosition - destination) * 264;
+return differenceInFeet;
+} else  if(currentPosition < destination) {
+    const differenceInFeet = (destination - currentPosition) * 264;
+    return differenceInFeet;
+    }
+
+}
+
+const calculatesFarePrice = (start, destination) => {
+
+    const distance = Math.abs(start - destination) * 264;
+    
+    if (distance <= 400) {
+    return 0;
+    }
+    else if(distance > 400 && distance < 2000) {
+        return (distance - 400) * 0.02;
+    } 
+    else if (distance > 2000 && distance < 2500) {
+        return 25;
+    } 
+    else { 
+        return 'cannot travel that far'; }
+}
